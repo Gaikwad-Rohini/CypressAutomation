f04f956bee169f3a61ae2bc80c0ab7d5c3afec7d
Rohini Doke <rohinidoke77@gmail.com>
2024-12-21 17:47:51 +0000

    cypress automation test
    cypress automation test

---
+++ cypress/e2e/VerifyWebUrl.cy.js
@@ -0,0 +1,11 @@
+describe("Verify the website url", () => {
+  it("verify title-positive", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM");
+  });
+
+  it("verify title-negative test", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM123");
+  });
+});
